25dfc107b84851fcae286629b74eb2c0c31465ce
steeve zozor <steeve.zozor@gmail.com>
2019-06-23 02:55:51 +0200

    Fin Esperanza condicional

--- Code_SZ/Rearreglo.m
+++ Code_SZ/Rearreglo.m
@@ -15,7 +15,13 @@ I = find(r <= 2^(1/d)); l = length(r)-length(I);I = I(end);
 Lp = [betainc(r(1:I).^2/(4^(1/d)),d/2,(nu-d)/2+1) ones(1,l)];
 
 % Lo mismo para la gausiana 
-Lg = gammainc((r.^2)/2/v,d/2);
+u = (r.^2)/2/v;
+Lg = gammainc(u,d/2);
+
+
+% Lo mismo para la Student-t
+u = r.^2/((nu-2)*v);
+Ls = 2/(d*beta(d/2,(nu+d)/2))*hypergeom([d/2 (nu+d)/2],d/2+1,-u);
 
 % Lo mismo para la uniforme de misa varianza
 %a = sqrt(3*(nu+3)/(nu+2));
@@ -29,5 +35,5 @@ Lg = gammainc((r.^2)/2/v,d/2);
 %Iar = [1-2*asin(1-r(1:I)/sqrt(2)/s)/pi ones(1,l)];
 
 
-plot(r,Lp,'b--',r,Lg,'r--')
+plot(r,Lp,'r-',r,Lg,'k-',r,Ls,'b-')
 %,r,Iur,'b--',r,erf(r/sqrt(2*v)),'r-',r,Iar,'k-',r,1-exp(-2*r/s),'k--')
